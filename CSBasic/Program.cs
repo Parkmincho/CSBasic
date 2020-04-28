c4c40bdc63934e834ecd344c89a4de19fb829104
Mincho Park <s2018s28@e-mirim.hs.kr>
2020-04-28 14:22:01 +0900

    C# 기본 연습

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -10,6 +10,30 @@ namespace CSBasic
     {
         static void Main(string[] args)
         {
+            //273;
+            var name = "박" + "민" + "초";
+            Console.Write("Hello C#");
+            int add = 2;
+            int Alpha;
+
+            Console.WriteLine("Hello Word");
+            Console.Write("Hello Word");
+            Console.Write("Hello Word");
+
+            Console.Write(52);
+            Console.WriteLine(7 % 3);
+            Console.WriteLine('A');
+            Console.WriteLine("A");
+
+
+            Console.WriteLine("한빛\t아카데미");
+            Console.WriteLine("한빛\n아카데미");
+            Console.WriteLine("\"\"\"");
+
+            Console.WriteLine("가" + "나" + "다");
+            Console.WriteLine("안녕"[100]);
+
+            Console.WriteLine(true);
         }
     }
 }
